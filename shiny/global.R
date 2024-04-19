251e4a7d8c610585f680ce0011bcf0c594aec7ce
Markus Haug <markushaug@Markuss-MacBook-Pro-2.local>
2024-04-19 11:32:05 +0300

    Added fixed domain colors to guy plot

--- shiny/global.R
+++ shiny/global.R
@@ -36,7 +36,8 @@ format_results = function(pathToResults, studyName){
     pivot_wider(id_cols = c(CONCEPT_ID, CONCEPT_NAME, HERITAGE, PREVALENCE_DIFFERENCE_RATIO, PREVALENCE), values_from = PRESENT, values_fill = 0, names_from = PERSON_ID, names_prefix = "PID_") %>%
     filter(CONCEPT_ID != 999999999) %>%
     mutate(CONCEPT_ID = as.character(CONCEPT_ID)) %>%
-    filter(CONCEPT_ID != "0")
+    filter(CONCEPT_ID != "0") %>%
+    mutate(CONCEPT_NAME = substr(CONCEPT_NAME, 1, 30))
 
   target_row_annotation = target_df %>%
     as.data.frame() %>%
@@ -93,20 +94,20 @@ plot_prevalence = function(filtered_target){
       ),
       by = "CONCEPT_ID"
     ) %>%
-    mutate(PREVALENCE = map_dbl(PRESENCE, mean)) %>%
-    mutate(AVERAGE_AGE = map_dbl(PRESENCE, ~ mean(filtered_target$target_col_annotation[names(.x), "AGE"][as.logical(.x)]))) %>%
+    mutate(PREVALENCE = purrr::map_dbl(PRESENCE, mean)) %>%
+    mutate(AVERAGE_AGE = purrr::map_dbl(PRESENCE, ~ mean(filtered_target$target_col_annotation[names(.x), "AGE"][as.logical(.x)]))) %>%
     mutate(AVERAGE_AGE_OVERALL = mean(filtered_target$target_col_annotation$AGE)) %>%
     mutate(AGE_DIFF = map(PRESENCE, ~ t.test(filtered_target$target_col_annotation[names(.x), "AGE"][as.logical(.x)], filtered_target$target_col_annotation[names(.x), "AGE"]))) %>%
-    mutate(AGE_DIFF_ESTIMATE = map_dbl(AGE_DIFF, ~ .x$estimate[1])) %>%
-    mutate(AGE_DIFF_LOW = map_dbl(AGE_DIFF, ~ .x$estimate[2] + .x$conf.int[1])) %>%
-    mutate(AGE_DIFF_HIGH = map_dbl(AGE_DIFF, ~ .x$estimate[2] + .x$conf.int[2])) %>%
-    mutate(AGE_DIFF_SIGNIFICANT = map_lgl(AGE_DIFF, ~ .x$p.value < 0.05)) %>%
-    mutate(MALE_PROP = map_dbl(PRESENCE, ~ mean(filtered_target$target_col_annotation[names(.x), "GENDER"][as.logical(.x)] == "Male"))) %>%
+    mutate(AGE_DIFF_ESTIMATE = purrr::map_dbl(AGE_DIFF, ~ .x$estimate[1])) %>%
+    mutate(AGE_DIFF_LOW = purrr::map_dbl(AGE_DIFF, ~ .x$estimate[2] + .x$conf.int[1])) %>%
+    mutate(AGE_DIFF_HIGH = purrr::map_dbl(AGE_DIFF, ~ .x$estimate[2] + .x$conf.int[2])) %>%
+    mutate(AGE_DIFF_SIGNIFICANT = purrr::map_lgl(AGE_DIFF, ~ .x$p.value < 0.05)) %>%
+    mutate(MALE_PROP = purrr::map_dbl(PRESENCE, ~ mean(filtered_target$target_col_annotation[names(.x), "GENDER"][as.logical(.x)] == "Male"))) %>%
     mutate(MALE_PROP_OVERALL = mean(filtered_target$target_col_annotation$GENDER == "Male")) %>%
     mutate(MALE_PROP_DIFF = map(PRESENCE, ~ t.test(filtered_target$target_col_annotation[names(.x), "GENDER"][as.logical(.x)] == "Male", filtered_target$target_col_annotation[names(.x), "GENDER"] == "Male"))) %>%
-    mutate(MALE_PROP_DIFF_ESTIMATE = map_dbl(MALE_PROP_DIFF, ~ .x$estimate[1])) %>%
-    mutate(MALE_PROP_DIFF_LOW = map_dbl(MALE_PROP_DIFF, ~ .x$estimate[2] + .x$conf.int[1])) %>%
-    mutate(MALE_PROP_DIFF_HIGH = map_dbl(MALE_PROP_DIFF, ~ .x$estimate[2] + .x$conf.int[2])) %>%
+    mutate(MALE_PROP_DIFF_ESTIMATE = purrr::map_dbl(MALE_PROP_DIFF, ~ .x$estimate[1])) %>%
+    mutate(MALE_PROP_DIFF_LOW = purrr::map_dbl(MALE_PROP_DIFF, ~ .x$estimate[2] + .x$conf.int[1])) %>%
+    mutate(MALE_PROP_DIFF_HIGH = purrr::map_dbl(MALE_PROP_DIFF, ~ .x$estimate[2] + .x$conf.int[2])) %>%
     mutate(MALE_PROP_DIFF_SIGNIFICANT = map_lgl(MALE_PROP_DIFF, ~ .x$p.value < 0.05))
 
   p1 = ggplot(plotdata, aes(x = PREVALENCE, y = CONCEPT_NAME, fill = PREVALENCE_DIFFERENCE_RATIO)) +
@@ -171,8 +172,8 @@ plot_heatmap = function(filtered_target){
     rownames_to_column("CONCEPT_ID") %>%
     group_by(HERITAGE) %>%
     summarize(CONCEPT_ID = list(CONCEPT_ID)) %>%
-    mutate(MATRIX = map(CONCEPT_ID, ~ filtered_target$target_matrix[.x, , drop = F])) %>%
-    mutate(MATRIX = map(
+    mutate(MATRIX = purrr::map(CONCEPT_ID, ~ filtered_target$target_matrix[.x, , drop = F])) %>%
+    mutate(MATRIX = purrr::map(
       MATRIX,
       function(x){
         if(nrow(x) > 1){
@@ -184,100 +185,55 @@ plot_heatmap = function(filtered_target){
     ))
 
   tm = do.call(rbind, reordering$MATRIX)
-  tm_gaps = map_int(reordering$MATRIX, nrow) %>% cumsum()
+  tm_gaps = purrr::map_int(reordering$MATRIX, nrow) %>% cumsum()
 
   # save(tm, target_row_annotation, person, file = str_c(pathToResults, "/matrix.RData"))
 
   tm2 = tm
   rownames(tm2) = filtered_target$target_row_annotation[rownames(tm), ]$CONCEPT_NAME
 
+  annotation_row = filtered_target$target_row_annotation %>%
+    remove_rownames() %>%
+    column_to_rownames("CONCEPT_NAME") %>%
+    select(-PREVALENCE_DIFFERENCE_RATIO)
+  annotation_col = filtered_target$target_col_annotation
 
-  # #######################
-  # # Define colors for Gender
-  # Define gender colors ----------
-  # gender_colors <- c("Male" = "blue", "Female" = "red")
-  #
-  # # Check unique values in the GENDER column
-  # print(unique(filtered_target$target_col_annotation$GENDER))
-  #
-  # # Apply the gender color mapping with a safeguard for unhandled values
-  # filtered_target$target_col_annotation$GENDER <- ifelse(
-  #   filtered_target$target_col_annotation$GENDER %in% names(gender_colors),
-  #   gender_colors[filtered_target$target_col_annotation$GENDER],
-  #   "grey"  # Default color for unhandled or NA values
-  # )
-  # # Define a color gradient for Age (example uses Age from 0 to 100) --------
-  # age_colors <- grDevices::colorRampPalette(c("white", "purple"))(101)  # Adjust the number as needed
-  #
-  # # Define colors for Heritage
-  # heritage_colors <- c(
-  #   "procedure_occurrence" = "purple",
-  #   "condition_occurrence" = "orange",
-  #   "drug_exposure" = "lightblue",
-  #   "measurement" = "pink",
-  #   "observation" = "brown"
-  # )
-  # # Map ages to colors (assuming Age is a numeric column)
-  # # Assuming your age data ranges from a minimum age to a maximum age
-  # min_age <- min(filtered_target$target_col_annotation$AGE)
-  # max_age <- max(filtered_target$target_col_annotation$AGE)
-  #
-  # # Create a color palette for age that fits the actual data range
-  # age_colors <- colorRampPalette(c("white", "purple"))(max_age - min_age + 1)
-  #
-  # # Scale the age values to the index of the color array
-  # scaled_ages <- round((filtered_target$target_col_annotation$AGE - min_age) / (max_age - min_age) * (length(age_colors) - 1)) + 1
-  # filtered_target$target_col_annotation$AGE <- age_colors[scaled_ages]
-  #
-  # # Map genders to colors
-  #
-  # # Map heritage to colors, handling missing values if they occur
-  # filtered_target$target_row_annotation$HERITAGE <- heritage_colors[filtered_target$target_row_annotation$HERITAGE]
-  #
-  #
-  # # Prepare annotations for columns and rows
-  # col_annotations <- as.data.frame(t(filtered_target$target_col_annotation))
-  # row_annotations <- as.data.frame(filtered_target$target_row_annotation)
-  #
-  #
-  # default_color <- "gray"
-  # col_annotations["GENDER",][is.na( col_annotations["GENDER",])] <- default_color
-  # row_annotations$HERITAGE[is.na(row_annotations$HERITAGE)] <- default_color
-  #
-  # # Create the heatmap
-  # # Re-attempt to plot with annotations
-  # pheatmap(
-  #   filtered_target$target_matrix,
-  #   annotation_col = col_annotations,
-  #   annotation_row = row_annotations,
-  #   cluster_cols = hclust(dist(t(filtered_target$target_matrix))),
-  #   cluster_rows = FALSE,
-  #   show_colnames = FALSE,
-  #   color = colorRampPalette(c("#e5f5f9", "#2ca25f"))(256),
-  #   gaps_row = c(0, cumsum(sapply(reordering$MATRIX, nrow))),
-  #   legend_breaks = c(0.25, 0.75),
-  #   legend_labels = c("Absent", "Present")
-  # )
+  # Heritage colors pre-defined
+  heritage_colors <- c(
+    procedure_occurrence = "darkblue",
+    condition_occurrence = "orange",
+    drug_exposure = "lightblue",
+    measurement = "pink",
+    observation = "brown"
+  )
 
-  ########################
+  active_gender_colors <-  c(Male = "purple", Female = "pink", Other = "lightblue")
 
-  #
-  pheatmap(
+  # Filter heritage colors based on what's present in annotation_row
+  active_heritage_colors <- heritage_colors[names(heritage_colors) %in% unique(annotation_row$HERITAGE)]
+  active_gender_colors <- active_gender_colors[names(active_gender_colors) %in% unique(annotation_col$GENDER)]
+
+
+  # Specify colors including a dynamic gradient for AGE
+  annotation_colors = list(
+    AGE = colorRampPalette(c("lightblue", "firebrick"))(length(unique(annotation_col$AGE))),
+    GENDER = active_gender_colors,
+    HERITAGE = active_heritage_colors,
+    PREVALENCE = colorRampPalette(c("white", "purple"))(length(unique(annotation_row$PREVALENCE)))
+  )
+
+
+  pheatmap::pheatmap(
     tm2,
     show_colnames = F,
-    annotation_row = filtered_target$target_row_annotation %>%
-      remove_rownames() %>%
-      column_to_rownames("CONCEPT_NAME") %>%
-      select(-PREVALENCE_DIFFERENCE_RATIO),
-    annotation_col = filtered_target$target_col_annotation,
+    annotation_row = annotation_row,
+    annotation_col = annotation_col,
+    annotation_colors = annotation_colors,
     cluster_cols = col_clustering,
     cluster_rows = FALSE,
     gaps_row = tm_gaps,
     color = c("#e5f5f9", "#2ca25f"),
     legend_breaks = c(0.25, 0.75),
     legend_labels = c("Absent", "Present")
-    # cellheight = 15,
-    # width = 10,
-    # filename = str_glue("{pathToResults}/heatmap_{name}.pdf")
   )
 }
