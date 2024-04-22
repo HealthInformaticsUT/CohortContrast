41297ca2d503977ef56468a96480783e86f99c24
Markus Haug <markushaug@Markuss-MacBook-Pro-2.local>
2024-04-22 13:39:19 +0300

    Some more refactoring

--- shiny/global.R
+++ shiny/global.R
@@ -1,70 +1,120 @@
 ############################### Global variables
-library(dplyr)
-library(tibble)
-library(tidyr)
+library(ggplot2)
 ############################## Shiny functions
 
 # Functions for data loading and formatting ------------------------------------
 format_results = function(pathToResults, studyName){
   load(stringr::str_c(pathToResults, "/tmp/datasets/", studyName, "_CC_medData.rdata"))
 
-  concepts = object$data_patients %>%
-    select(CONCEPT_ID, CONCEPT_NAME, HERITAGE) %>%
-    distinct() %>%
-    filter(CONCEPT_ID != 0) %>%
-    inner_join(object$data_features, by = c("CONCEPT_ID", "CONCEPT_NAME")) %>%
-    filter(!is.na(PREVALENCE_DIFFERENCE_RATIO))
+  concepts = dplyr::filter(
+    dplyr::inner_join(
+      dplyr::filter(dplyr::distinct(
+        dplyr::select(object$data_patients, CONCEPT_ID, CONCEPT_NAME, HERITAGE)
+      ), CONCEPT_ID != 0),
+      object$data_features,
+      by = c("CONCEPT_ID", "CONCEPT_NAME")
+    ) ,
+    !is.na(PREVALENCE_DIFFERENCE_RATIO)
+  )
 
-  target = object$data_patients %>%
-    filter(COHORT_DEFINITION_ID == 2) %>%
-    select(-COHORT_DEFINITION_ID) %>%
-    left_join(concepts, by = c("CONCEPT_ID", "CONCEPT_NAME", "HERITAGE")) %>%
-    filter(!is.na(PREVALENCE_DIFFERENCE_RATIO))
+  target = dplyr::filter(
+    dplyr::left_join(
+      dplyr::select(
+        dplyr::filter(object$data_patients, COHORT_DEFINITION_ID == 2),
+        -COHORT_DEFINITION_ID
+      ),
+      concepts,
+      by = c("CONCEPT_ID", "CONCEPT_NAME", "HERITAGE")
+    ),
+    !is.na(PREVALENCE_DIFFERENCE_RATIO)
+  )
 
-  target_df = target %>%
-    mutate(NPATIENTS = length(unique(PERSON_ID))) %>%
-    mutate(PRESENT = 1) %>%
-    group_by(CONCEPT_ID) %>%
-    mutate(NCONCEPTS = n()) %>%
-    ungroup() %>%
-    mutate(PREVALENCE = NCONCEPTS / NPATIENTS) %>%
-    bind_rows(
-      target %>%
-        group_by(PERSON_ID) %>%
-        summarize(CONCEPT_ID = 999999999, CONCEPT_NAME = "None", PREVALENCE = 99999, HERITAGE = "none", PREVALENCE_DIFFERENCE_RATIO = -1)
-    ) %>%
-    pivot_wider(id_cols = c(CONCEPT_ID, CONCEPT_NAME, HERITAGE, PREVALENCE_DIFFERENCE_RATIO, PREVALENCE), values_from = PRESENT, values_fill = 0, names_from = PERSON_ID, names_prefix = "PID_") %>%
-    filter(CONCEPT_ID != 999999999) %>%
-    mutate(CONCEPT_ID = as.character(CONCEPT_ID)) %>%
-    filter(CONCEPT_ID != "0") %>%
-    mutate(CONCEPT_NAME = substr(CONCEPT_NAME, 1, 30))
+  target_df = dplyr::mutate(dplyr::filter(
+    dplyr::mutate(
+      dplyr::filter(
+        tidyr::pivot_wider(
+          dplyr::bind_rows(
+            dplyr::mutate(dplyr::ungroup(
+              dplyr::mutate(dplyr::group_by(
+                dplyr::mutate(dplyr::mutate(target, NPATIENTS = length(unique(
+                  PERSON_ID
+                ))), PRESENT = 1), CONCEPT_ID
+              ), NCONCEPTS = dplyr::n())
+            ), PREVALENCE = NCONCEPTS / NPATIENTS),
+            dplyr::summarize(
+              dplyr::group_by(target, PERSON_ID),
+              CONCEPT_ID = 999999999,
+              CONCEPT_NAME = "None",
+              PREVALENCE = 99999,
+              HERITAGE = "none",
+              PREVALENCE_DIFFERENCE_RATIO = -1
+            )
+          ),
+          id_cols = c(
+            CONCEPT_ID,
+            CONCEPT_NAME,
+            HERITAGE,
+            PREVALENCE_DIFFERENCE_RATIO,
+            PREVALENCE
+          ),
+          values_from = PRESENT,
+          values_fill = 0,
+          names_from = PERSON_ID,
+          names_prefix = "PID_"
+        ) ,
+        CONCEPT_ID != 999999999
+      ),
+      CONCEPT_ID = as.character(CONCEPT_ID)
+    ),
+    CONCEPT_ID != "0"
+  ),
+  CONCEPT_NAME = substr(CONCEPT_NAME, 1, 30))
 
-  target_row_annotation = target_df %>%
-    as.data.frame() %>%
-    column_to_rownames("CONCEPT_ID") %>%
-    select(-starts_with("PID_"))
 
-  target_matrix = target_df %>%
-    as.data.frame() %>%
-    column_to_rownames("CONCEPT_ID") %>%
-    select(starts_with("PID_")) %>%
-    as.matrix()
+  # TODO
+  target_row_annotation = dplyr::select(
+    tibble::column_to_rownames(as.data.frame(target_df), "CONCEPT_ID"),
+    -dplyr::starts_with("PID_")
+  )
+
+  target_matrix = as.matrix(dplyr::select(
+    tibble::column_to_rownames(as.data.frame(target_df), "CONCEPT_ID"),
+    dplyr::starts_with("PID_")
+  ))
 
   # Demographics
-  target_col_annotation = object$data_person %>%
-    inner_join(
-      object$data_initial %>%
-        filter(COHORT_DEFINITION_ID == 2) %>%
-        select(PERSON_ID = SUBJECT_ID, COHORT_START_DATE),
-      by = "PERSON_ID"
-    ) %>%
-    mutate(AGE = floor(as.numeric(COHORT_START_DATE - lubridate::as_date(stringr::str_glue("{YEAR_OF_BIRTH}-01-01"))) / 365.25)) %>%
-    select(PERSON_ID, GENDER_CONCEPT_ID, AGE) %>%
-    mutate(GENDER = factor(GENDER_CONCEPT_ID, levels = c(8507, 8532), labels = c("Male", "Female"))) %>%
-    mutate(PERSON_ID = stringr::str_c("PID_", PERSON_ID)) %>%
-    as.data.frame() %>%
-    column_to_rownames("PERSON_ID") %>%
-    select(-GENDER_CONCEPT_ID)
+  target_col_annotation =  dplyr::select(tibble::column_to_rownames(as.data.frame(
+    dplyr::mutate(
+      dplyr::mutate(
+        dplyr::select(
+          dplyr::mutate(
+            dplyr::inner_join(
+              object$data_person,
+              dplyr::select(
+                dplyr::filter(object$data_initial, COHORT_DEFINITION_ID == 2),
+                PERSON_ID = SUBJECT_ID,
+                COHORT_START_DATE
+              ),
+              by = "PERSON_ID"
+            ),
+            AGE = floor(as.numeric(
+              COHORT_START_DATE - lubridate::as_date(stringr::str_glue("{YEAR_OF_BIRTH}-01-01"))
+            ) / 365.25)
+          ),
+          PERSON_ID,
+          GENDER_CONCEPT_ID,
+          AGE
+        ) ,
+        GENDER = factor(
+          GENDER_CONCEPT_ID,
+          levels = c(8507, 8532),
+          labels = c("Male", "Female")
+        )
+      ),
+      PERSON_ID = stringr::str_c("PID_", PERSON_ID)
+    )
+  ) , "PERSON_ID"),
+  -GENDER_CONCEPT_ID)
 
   res = list(target_matrix = target_matrix, target_row_annotation = target_row_annotation, target_col_annotation = target_col_annotation)
 
@@ -73,10 +123,16 @@ format_results = function(pathToResults, studyName){
 
 filter_target = function(target, prevalence_thereshold, prevalence_ratio_threshold, domain){
   res = target
-  res$target_row_annotation = target$target_row_annotation %>%
-    filter(PREVALENCE > prevalence_thereshold) %>%
-    filter(PREVALENCE_DIFFERENCE_RATIO > prevalence_ratio_threshold) %>%
-    filter(HERITAGE %in% domain)
+  res$target_row_annotation =  dplyr::filter(
+    dplyr::filter(
+      dplyr::filter(
+        target$target_row_annotation ,
+        PREVALENCE > prevalence_thereshold
+      ),
+      PREVALENCE_DIFFERENCE_RATIO > prevalence_ratio_threshold
+    ),
+    HERITAGE %in% domain
+  )
 
   res$target_matrix = target$target_matrix[rownames(res$target_row_annotation), ]
 
@@ -84,31 +140,54 @@ filter_target = function(target, prevalence_thereshold, prevalence_ratio_thresho
 }
 
 plot_prevalence = function(filtered_target){
-  plotdata = filtered_target$target_row_annotation %>%
-    rownames_to_column("CONCEPT_ID") %>%
-    as_tibble() %>%
-    left_join(
-      tibble(
-        CONCEPT_ID = rownames(filtered_target$target_matrix),
-        PRESENCE = lapply(seq_len(nrow(filtered_target$target_matrix)), function(i) filtered_target$target_matrix[i,])
+  plotdata =
+    dplyr::mutate(
+      dplyr::left_join(
+        tibble::as_tibble(
+          tibble::rownames_to_column(filtered_target$target_row_annotation, "CONCEPT_ID")
+        ),
+        tibble::tibble(
+          CONCEPT_ID = rownames(filtered_target$target_matrix),
+          PRESENCE = lapply(seq_len(nrow(
+            filtered_target$target_matrix
+          )), function(i)
+            filtered_target$target_matrix[i, ])
+        ),
+        by = "CONCEPT_ID"
       ),
-      by = "CONCEPT_ID"
-    ) %>%
-    mutate(PREVALENCE = purrr::map_dbl(PRESENCE, mean)) %>%
-    mutate(AVERAGE_AGE = purrr::map_dbl(PRESENCE, ~ mean(filtered_target$target_col_annotation[names(.x), "AGE"][as.logical(.x)]))) %>%
-    mutate(AVERAGE_AGE_OVERALL = mean(filtered_target$target_col_annotation$AGE)) %>%
-    mutate(AGE_DIFF = map(PRESENCE, ~ t.test(filtered_target$target_col_annotation[names(.x), "AGE"][as.logical(.x)], filtered_target$target_col_annotation[names(.x), "AGE"]))) %>%
-    mutate(AGE_DIFF_ESTIMATE = purrr::map_dbl(AGE_DIFF, ~ .x$estimate[1])) %>%
-    mutate(AGE_DIFF_LOW = purrr::map_dbl(AGE_DIFF, ~ .x$estimate[2] + .x$conf.int[1])) %>%
-    mutate(AGE_DIFF_HIGH = purrr::map_dbl(AGE_DIFF, ~ .x$estimate[2] + .x$conf.int[2])) %>%
-    mutate(AGE_DIFF_SIGNIFICANT = purrr::map_lgl(AGE_DIFF, ~ .x$p.value < 0.05)) %>%
-    mutate(MALE_PROP = purrr::map_dbl(PRESENCE, ~ mean(filtered_target$target_col_annotation[names(.x), "GENDER"][as.logical(.x)] == "Male"))) %>%
-    mutate(MALE_PROP_OVERALL = mean(filtered_target$target_col_annotation$GENDER == "Male")) %>%
-    mutate(MALE_PROP_DIFF = map(PRESENCE, ~ t.test(filtered_target$target_col_annotation[names(.x), "GENDER"][as.logical(.x)] == "Male", filtered_target$target_col_annotation[names(.x), "GENDER"] == "Male"))) %>%
-    mutate(MALE_PROP_DIFF_ESTIMATE = purrr::map_dbl(MALE_PROP_DIFF, ~ .x$estimate[1])) %>%
-    mutate(MALE_PROP_DIFF_LOW = purrr::map_dbl(MALE_PROP_DIFF, ~ .x$estimate[2] + .x$conf.int[1])) %>%
-    mutate(MALE_PROP_DIFF_HIGH = purrr::map_dbl(MALE_PROP_DIFF, ~ .x$estimate[2] + .x$conf.int[2])) %>%
-    mutate(MALE_PROP_DIFF_SIGNIFICANT = map_lgl(MALE_PROP_DIFF, ~ .x$p.value < 0.05))
+      PREVALENCE = purrr::map_dbl(PRESENCE, mean),
+      AVERAGE_AGE = purrr::map_dbl(PRESENCE, ~ mean(
+        filtered_target$target_col_annotation[names(.x), "AGE"][as.logical(.x)]
+      )),
+      AVERAGE_AGE_OVERALL = mean(filtered_target$target_col_annotation$AGE),
+      AGE_DIFF = map(
+        PRESENCE,
+        ~ t.test(
+          filtered_target$target_col_annotation[names(.x), "AGE"][as.logical(.x)],
+          filtered_target$target_col_annotation[names(.x), "AGE"]
+        )
+      ),
+      AGE_DIFF_ESTIMATE = purrr::map_dbl(AGE_DIFF, ~ .x$estimate[1]),
+      AGE_DIFF_LOW = purrr::map_dbl(AGE_DIFF, ~ .x$estimate[2] + .x$conf.int[1]),
+      AGE_DIFF_HIGH = purrr::map_dbl(AGE_DIFF, ~ .x$estimate[2] + .x$conf.int[2]),
+      AGE_DIFF_SIGNIFICANT = purrr::map_lgl(AGE_DIFF, ~ .x$p.value < 0.05),
+      MALE_PROP = purrr::map_dbl(
+        PRESENCE,
+        ~ mean(filtered_target$target_col_annotation[names(.x), "GENDER"][as.logical(.x)] == "Male")
+      ),
+      MALE_PROP_OVERALL = mean(filtered_target$target_col_annotation$GENDER == "Male"),
+      MALE_PROP_DIFF = map(
+        PRESENCE,
+        ~ t.test(
+          filtered_target$target_col_annotation[names(.x), "GENDER"][as.logical(.x)] == "Male",
+          filtered_target$target_col_annotation[names(.x), "GENDER"] == "Male"
+        )
+      ),
+      MALE_PROP_DIFF_ESTIMATE = purrr::map_dbl(MALE_PROP_DIFF, ~ .x$estimate[1]),
+      MALE_PROP_DIFF_LOW = purrr::map_dbl(MALE_PROP_DIFF, ~ .x$estimate[2] + .x$conf.int[1]),
+      MALE_PROP_DIFF_HIGH = purrr::map_dbl(MALE_PROP_DIFF, ~ .x$estimate[2] + .x$conf.int[2]),
+      MALE_PROP_DIFF_SIGNIFICANT = map_lgl(MALE_PROP_DIFF, ~ .x$p.value < 0.05)
+    )
 
   p1 = ggplot(plotdata, aes(x = PREVALENCE, y = CONCEPT_NAME, fill = PREVALENCE_DIFFERENCE_RATIO)) +
     geom_bar(stat = "identity") +
@@ -165,37 +244,45 @@ plot_prevalence = function(filtered_target){
 
 plot_heatmap = function(filtered_target){
   # Patient clustering
-  col_clustering = hclust(dist(t(filtered_target$target_matrix)))
+  col_clustering = stats::hclust(stats::dist(t(filtered_target$target_matrix)))
 
   # Row reordering
-  reordering = filtered_target$target_row_annotation %>%
-    rownames_to_column("CONCEPT_ID") %>%
-    group_by(HERITAGE) %>%
-    summarize(CONCEPT_ID = list(CONCEPT_ID)) %>%
-    mutate(MATRIX = purrr::map(CONCEPT_ID, ~ filtered_target$target_matrix[.x, , drop = F])) %>%
-    mutate(MATRIX = purrr::map(
-      MATRIX,
-      function(x){
-        if(nrow(x) > 1){
-          x = x[hclust(dist(x))$order, ]
-        }
+  reordering = dplyr::mutate(
+      dplyr::mutate(
+        dplyr::summarize(
+          dplyr::group_by(
+            tibble::rownames_to_column(filtered_target$target_row_annotation, "CONCEPT_ID"),
+            HERITAGE
+          ),
+          CONCEPT_ID = list(CONCEPT_ID)
+        ),
+        MATRIX = purrr::map(CONCEPT_ID, ~ filtered_target$target_matrix[.x, , drop = F])
+      ),
+      MATRIX = purrr::map(MATRIX,
+                          function(x) {
+                            if (nrow(x) > 1) {
+                              x = x[stats::hclust(stats::dist(x))$order,]
+                            }
 
-        return(x)
-      }
-    ))
+                            return(x)
+                          })
+    )
 
   tm = do.call(rbind, reordering$MATRIX)
-  tm_gaps = purrr::map_int(reordering$MATRIX, nrow) %>% cumsum()
+  tm_gaps = cumsum(purrr::map_int(reordering$MATRIX, nrow))
 
   # save(tm, target_row_annotation, person, file = str_c(pathToResults, "/matrix.RData"))
 
   tm2 = tm
   rownames(tm2) = filtered_target$target_row_annotation[rownames(tm), ]$CONCEPT_NAME
 
-  annotation_row = filtered_target$target_row_annotation %>%
-    remove_rownames() %>%
-    column_to_rownames("CONCEPT_NAME") %>%
-    select(-PREVALENCE_DIFFERENCE_RATIO)
+  annotation_row = dplyr::select(
+    tibble::column_to_rownames(
+      tibble::remove_rownames(filtered_target$target_row_annotation) ,
+      "CONCEPT_NAME"
+    ),
+    -PREVALENCE_DIFFERENCE_RATIO
+  )
   annotation_col = filtered_target$target_col_annotation
 
   # Heritage colors pre-defined
@@ -216,10 +303,10 @@ plot_heatmap = function(filtered_target){
 
   # Specify colors including a dynamic gradient for AGE
   annotation_colors = list(
-    AGE = colorRampPalette(c("lightblue", "firebrick"))(length(unique(annotation_col$AGE))),
+    AGE = grDevices::colorRampPalette(c("lightblue", "firebrick"))(length(unique(annotation_col$AGE))),
     GENDER = active_gender_colors,
     HERITAGE = active_heritage_colors,
-    PREVALENCE = colorRampPalette(c("white", "purple"))(length(unique(annotation_row$PREVALENCE)))
+    PREVALENCE = grDevices::colorRampPalette(c("white", "purple"))(length(unique(annotation_row$PREVALENCE)))
   )
 
 
