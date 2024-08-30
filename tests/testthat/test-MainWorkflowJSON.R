72c655c7c8e9c481aa3789577a20371acc8e634f
haugmarkus <markushaug123@gmail.com>
2024-08-30 10:40:22 +0300

    #28 created cohortFromJSON + test

---
+++ tests/testthat/test-MainWorkflowJSON.R
@@ -0,0 +1,53 @@
+library(testthat)
+library(CohortContrast)
+
+test_that("Created features table is correct.", {
+  pathToResults <<- getwd() #pathToResults = paste(getwd(), "/tests",sep="")
+
+  ################################################################################
+  #
+  # Database credentials
+  #
+  ################################################################################
+  pathToJSON <- paste(pathToResults, '/inst/JSON/', sep = '')
+
+  db <- DBI::dbConnect(duckdb::duckdb(), dbdir = CDMConnector::eunomia_dir("GiBleed"))
+
+  cdm <- CDMConnector::cdm_from_con(db, cdm_name = "eunomia", cdm_schema = "main", write_schema = "main")
+
+  targetTable <- cohortFromJSON(pathToJSON = pathToJSON, cdm = cdm)
+  controlTable <- createControlCohortInverse(cdm = cdm, targetTable = targetTable)
+
+  cdm <- createCohortContrastCdm(
+    cdm = cdm,
+    targetTable = targetTable,
+    controlTable = controlTable
+  )
+  ################################################################################
+  #
+  # Run the study
+  #
+  ################################################################################
+  data = CohortContrast(
+    cdm = cdm,
+    pathToResults =  getwd(),
+    domainsIncluded = c("Drug"),
+    prevalenceCutOff = 0,
+    topK = 15, # Number of features to export
+    presenceFilter = FALSE, # 0-1, percentage of people who must have the chosen feature present
+    complementaryMappingTable = FALSE, # A table for manual concept_id and concept_name mapping (merge)
+    createC2TInput = TRUE,
+    runZTests = FALSE,
+    runLogitTests = FALSE,
+    createOutputFiles = FALSE)
+
+  expect_equal(length(data$resultList$selectedFeatures$CONCEPT_NAME) == 15, TRUE)
+  expect_equal(as.numeric(data$data_features[data$data_features$CONCEPT_NAME == "atorvastatin 80 MG Oral Tablet", 3]) == 19, TRUE)
+  expect_equal(nrow(data$resultList$trajectoryData) == 3332, TRUE)
+  expect_equal(nrow(data$data_initial) == 1660, TRUE)
+  expect_equal(nrow(data$data_person) == 2694, TRUE)
+  expect_equal(nrow(data$data_patients) == 1041, TRUE)
+
+  DBI::dbDisconnect(db)
+})
+#> Test passed 🥇
