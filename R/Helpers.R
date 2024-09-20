458be63fc2f9351093909c88891b5c168ab33edd
haugmarkus <markushaug123@gmail.com>
2024-09-20 14:42:27 +0300

    fixed relative path bug

--- R/Helpers.R
+++ R/Helpers.R
@@ -376,3 +376,32 @@ print.CohortContrastObject <- function(x, ...) {
     }
   }
 }
+
+#' Convert a relative path to an absolute path
+#'
+#' This internal function converts a given relative path into an absolute path
+#' using the built-in `normalizePath()` function.
+#'
+#' @param path A string representing the file or directory path, which may be relative or absolute.
+#'
+#' @return A string with the absolute path. If the file or directory does not exist,
+#'         `normalizePath()` will throw an error due to `mustWork = TRUE`.
+#'
+#' @details The function utilizes `normalizePath()` to handle both Windows and Unix-based
+#'          systems. It converts the provided path, even if it is relative, into
+#'          an absolute path. It ensures that paths are formatted with forward slashes
+#'          on Windows systems by setting `winslash = "/"`.
+#'
+#' @keywords internal
+#'
+#' @examples
+#' \dontrun{
+#'   relative_path <- "relative/path/to/file.txt"
+#'   absolute_path <- convertToAbsolutePath(relative_path)
+#'   print(absolute_path)
+#' }
+convertToAbsolutePath <- function(path) {
+  # normalizePath() will convert a relative path to an absolute one
+  absolute_path <- normalizePath(path, winslash = "/", mustWork = TRUE)
+  return(absolute_path)
+}
