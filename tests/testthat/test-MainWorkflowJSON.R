a72591fa5d177bfe744e4a20749bcf911125ef69
haugmarkus <markushaug123@gmail.com>
2024-10-21 08:31:19 -0400

    logit test bug fix

--- tests/testthat/test-MainWorkflowJSON.R
+++ tests/testthat/test-MainWorkflowJSON.R
@@ -44,6 +44,42 @@ test_that("Created features table is correct.", {
   expect_equal(nrow(data$data_person) == 830, TRUE)
   expect_equal(nrow(data$data_patients) == 3199, TRUE)
 
+
+
+  controlTable <- createControlCohortMatching(cdm = cdm, targetTable = targetTable, ratio = 5)
+  controlTable <- controlTable %>% dplyr::mutate(cohort_end_date = cohort_start_date + 10)
+
+  ################################################################################
+  #
+  # Run the study
+  #
+  ################################################################################
+  data = CohortContrast(
+    cdm = cdm,
+    targetTable = targetTable,
+    controlTable = controlTable,
+    pathToResults =getwd(), #paste(getwd(), '/tests/testthat', sep = ''),
+    domainsIncluded = c(
+      "Drug",
+      "Condition",
+      "Measurement",
+      "Observation",
+      "Procedure",
+      "Visit",
+      "Visit detail"
+    ),
+    prevalenceCutOff = 0,
+    topK = FALSE, # Number of features to export
+    presenceFilter = FALSE, # 0-1, percentage of people who must have the chosen feature present
+    complementaryMappingTable = FALSE, # A table for manual concept_id and concept_name mapping (merge)
+    runZTests = TRUE,
+    runLogitTests = TRUE,
+    createOutputFiles = FALSE,
+    numCores = 1)
+
+  expect_equal(sum(data$data_features$ZTEST) == 1, TRUE)
+  expect_equal(sum(data$data_features$LOGITTEST) == 0, TRUE)
+
   DBI::dbDisconnect(db)
 })
 #> Test passed 🥇
